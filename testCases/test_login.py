b93ff335f45b69f574afc5d35e90afab9d7091fb
unknown <rajeshwari.bannetti1@gmail.com>
2021-09-02 11:02:04 +0530

    First commit

---
+++ testCases/test_login.py
@@ -0,0 +1,33 @@
+import pytest
+from selenium import webdriver
+from PageObjects.LoginPage import LoginPage
+from utilities.readProperties import ReadConfig
+
+
+class Test_001_Login:
+
+    baseURL = ReadConfig.getApplicationURL()
+    username = ReadConfig.getUsername()
+    password = ReadConfig.getPassword()
+
+    def test_homePageTitle(self, setup):
+
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        act_title = self.driver.title
+        self.driver.close()
+        if act_title == "Log in to Jai Kisan":
+            assert True
+        else:
+            assert False
+
+    def test_login(self, setup):
+        self.driver = setup
+        self.driver.get(self.baseURL)
+        self.lp = LoginPage(self.driver)
+        self.lp.setUserName(self.username)
+        self.lp.setPassword(self.password)
+        self.lp.clickLogin()
+        act_title = self.driver.title
+        self.driver.close()
+
