14cb05276f6dab378e753246a6a4f3694f6cc3c3
RajeshwariBannetti <rajeshwari.bannetti@jai-kisan.com>
2021-09-27 10:56:38 +0530

    Network changes

--- pageObjects/Filters.py
+++ pageObjects/Filters.py
@@ -25,12 +25,16 @@ class Filter:
     Date_Table_xpath = "//body/anchors-root[1]/anchors-anchors-container[1]/anchors-applications[1]/div[1]/div[" \
                        "2]/anchors-table[1]/div[1]/anchors-table-filters[1]/div[1]/div[2]/form[1]/div[2]/div[1]/div[" \
                        "1]/div[1]/mat-form-field[1]/div[1]/div[1]/div[3]/mat-date-range-input[1] "
-    DateRangeFrom_xpath = "//input[@id='mat-date-range-input-1']"
+    DateRangeFrom_xpath = "//input[@id='mat-date-range-input-0']"
     DateRangeTo_xpath = "//body/anchors-root[1]/anchors-anchors-container[1]/anchors-applications[1]/div[1]/div[" \
                         "2]/anchors-table[1]/div[1]/anchors-table-filters[1]/div[1]/div[2]/form[1]/div[2]/div[1]/div[" \
                         "1]/div[1]/mat-form-field[1]/div[1]/div[1]/div[3]/mat-date-range-input[1]/div[1]/div[" \
                         "2]/input[1] "
     btnSave_xpath = "//span[contains(text(),'Save')]"
+    tableSearchResult = "//body/anchors-root[1]/anchors-anchors-container[1]/anchors-applications[1]/div[1]/div[" \
+                        "2]/anchors-table[1]/div[1]/div[3]/anchors-table-card[1]/mat-card[1] "
+    tableRows_xpath = "//table[@id = 'cdk-drop-list-0']//tbody/tr"
+    tableColumn_xpath = "//table[@id = 'cdk-drop-list-0']//tbody/tr/td"
 
     def __init__(self, driver):
         self.driver = driver
@@ -42,7 +46,10 @@ class Filter:
         self.driver.find_element_by_xpath(self.placeholder_FirmType_xpath).click()
 
     def listfirm(self):
-        self.driver.find_element_by_xpath(self.lstFirmType_LimitedLiabilityCompany_xpath).click()
+        self.driver.find_element_by_xpath(self.lstFirmType_PublicLimitedCompany_xpath).click()
+
+    # def listfirm(self):
+    #     self.driver.find_element_by_xpath(self.lstFirmType_LimitedLiabilityCompany_xpath).click()
 
     # def setFirmType(self):
     #     self.driver.find_element_by_xpath(self.placeholder_FirmType_xpath).click()
@@ -90,16 +97,22 @@ class Filter:
     #     self.driver.execute_script("arguments[0].click();", self.firmtype)
 
     def DateRangeIp(self):
-        self.clickOnDate = self.driver.find_element_by_xpath(self.Date_Table_xpath).click()
+        self.driver.find_element_by_xpath(self.Date_Table_xpath).click()
 
     def DateRangeFrom(self, fromDate):
-        self.selectfrom = self.driver.find_element_by_xpath(self.DateRangeFrom_xpath).send_keys(fromDate)
+        self.driver.find_element_by_xpath(self.DateRangeFrom_xpath).send_keys(fromDate)
 
     def DateRangeTo(self, toDate):
-        self.selectfrom = self.driver.find_element_by_xpath(self.DateRangeTo_xpath).send_keys(toDate)
+        self.driver.find_element_by_xpath(self.DateRangeTo_xpath).send_keys(toDate)
 
     def Savebutton(self):
-        self.clickbutton = self.driver.find_element_by_xpath(self.btnSave_xpath).click()
+        self.driver.find_element_by_xpath(self.btnSave_xpath).click()
 
-        time.sleep(3)
-        self.driver.execute_script("arguments[0].click();", self.listFirmType)
+    def Searchresult(self):
+        self.driver.find_element_by_xpath(self.tableSearchResult).click()
+
+    def getNoOfRows(self):
+        row_count = len(self.driver.find_element_by_xpath(self.tableRows_xpath))
+
+    def getNoOfCloumns(self):
+        return len(self.driver.find_element_by_xpath(self.tableColumn_xpath))
